1b4058de8f3d1352423a8ee13b8f2235f0b17b97
dabby04 <97193496+dabby04@users.noreply.github.com>
2025-02-15 10:24:40 -0700

    Added Scoring system

--- Assets/Scripts/Gutter.cs
+++ Assets/Scripts/Gutter.cs
@@ -4,14 +4,19 @@ public class Gutter : MonoBehaviour
 {
     private void OnTriggerEnter(Collider triggeredBody)
     {
-        Debug.Log(triggeredBody.gameObject.name);
-        Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
+        if (triggeredBody.CompareTag("Ball"))
+        {
 
-        float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
 
-        ballRigidBody.linearVelocity = Vector3.zero;
-        ballRigidBody.angularVelocity = Vector3.zero;
+            Debug.Log(triggeredBody.gameObject.name);
+            Rigidbody ballRigidBody = triggeredBody.GetComponent<Rigidbody>();
 
-        ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
+            float velocityMagnitude = ballRigidBody.linearVelocity.magnitude;
+
+            ballRigidBody.linearVelocity = Vector3.zero;
+            ballRigidBody.angularVelocity = Vector3.zero;
+
+            ballRigidBody.AddForce(transform.forward * velocityMagnitude, ForceMode.VelocityChange);
+        }
     }
 }
